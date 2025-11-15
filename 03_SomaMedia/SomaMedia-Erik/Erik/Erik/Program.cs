fa30f63d578acb9463021e19d542dbf789610118
Erik Silva Maia <erikm384@gmail.com>
2025-11-15 19:39:44 -0300

    Feat: Add mean

---
+++ 03_SomaMedia/SomaMedia-Erik/Erik/Erik/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var mean = new Mean();
+
+class Mean
+{
+    private List<float> _numbers = new List<float>();
+
+    public void Add(float number)
+    {
+        _numbers.Add(number);
+    }
+
+    public float calculate()
+    {
+        if (_numbers.Count == 0)
+        {
+            Console.WriteLine("sem valores cadastrados");
+            return 0;
+        }
+        float sum = 0;
+        foreach (var number in _numbers)
+        {
+            sum += number;
+        }
+
+        var mean = sum / _numbers.Count;
+        return mean;
+    }
+    
+    public Mean()
+    {
+        Console.Write("Coloque o valores cadastrados: ");
+        try
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                var atual = float.Parse(Console.ReadLine());
+                Add(atual);
+            }
+
+            Console.WriteLine("Limite maximo de 10");
+        }
+        finally
+        {
+            Console.WriteLine(calculate());
+        }
+    }
+    
+}
